6995d5f0140e9152e21e4433fc46e372adaa7e9a
dieboss <s41115208@gmail.com>
2025-01-16 18:14:50 +0800

    變更bs路徑

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './assets/all.scss'
 import App from './App.jsx'
 
